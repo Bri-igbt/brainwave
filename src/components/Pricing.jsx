47a8a9b3ce831bcbe5e1ce3ae522469b4728fcfd
mac <mac@macs-MacBook-Pro.local>
2025-07-14 16:29:38 +0100

    Initial First Commit

---
+++ src/components/Pricing.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Section from './Section'
+import { smallSphere, stars } from '../assets'
+import Heading from './Heading'
+import PricingList from './PricingList'
+import { LeftLine, RightLine, Rightline } from './design/Pricing'
+
+const Pricing = () => {
+    return (
+        <Section className="overflow-hidden" id="pricing">
+            <div className='container z-2 relative'>
+                <div className='relative justify-center hidden mb-[6.6rem] lg:flex'>
+                    <img 
+                        src={smallSphere} 
+                        alt="sphere" 
+                        className='relative z-1'
+                        width={255}
+                        height={255}
+                    />
+
+                    <div className='absolute top-1/2 left-1/2 w-[60rem] -translate-y-1/2 pointer-events-none'>
+                        <img 
+                            src={stars} 
+                            alt="stars" 
+                            className='w-full'
+                            width={950}
+                            height={400}
+                        />
+                    </div>
+                </div>
+
+                <Heading 
+                    tag="Get started with Brainwave"
+                    title="Pay once, use forever"
+                />
+
+                <div className='relative'>
+                    <PricingList />
+                    <LeftLine />
+                    <RightLine />
+                </div>
+            </div>
+        </Section>
+    )
+}
+
+export default Pricing
---
+++ src/components/PricingList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { pricing } from '../constants'
+
+const PricingList = () => {
+    return (
+        <div className='flex gap-[1rem] max-lg:flex-wrap'>
+            {pricing}
+        </div>
+    )
+}
+
+export default PricingList
